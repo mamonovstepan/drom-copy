2915e2014bd3c42427a7de0cf0981134bafeaf0e
Stepan Mamonov <stepan.mamonov.1406@gmail.com>
2024-01-19 22:36:11 +0800

    подключил статику и медиа

--- dromcore/settings.py
+++ dromcore/settings.py
@@ -88,6 +88,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = (BASE_DIR / 'static')
+
+MEDIA_ROOT = (BASE_DIR / 'media')
+MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
