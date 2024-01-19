2ca39aa5a1283ccddc6e444ad7fbab3ad7bfe6bc
Stepan Mamonov <stepan.mamonov.1406@gmail.com>
2024-01-19 22:41:38 +0800

    написал и добавил в админ-панель модели категорий транспорта и запчастей

--- spareparts/apps.py
+++ spareparts/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SparepartsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'spareparts'
+    verbose_name = 'Запчасти'
--- spareparts/models.py
+++ spareparts/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Модель хранящая тип транспортного средства (авто, мото и т.д.)
+class TypeOfTransport(models.Model):
+    type = models.CharField(verbose_name='Тип транспорта', max_length=10)
+
+    class Meta:
+        verbose_name = 'Тип транспорта'
+        verbose_name_plural = 'Типы транспорта'
+
+    def __str__(self):
+        return self.type
+
+
+# Функция для составления пути сохранения фото в соответствии с пользователем
+def upload_path(instance, filename):
+    return f'SpareParts/{instance.author.username}/{filename}'
+
+
+# Модель хранящая объявление о продаже запасной части
+class SparePart(models.Model):
+    CONDITION = [
+        ('new', 'Новый'),
+        ('old', 'Б/У'),
+    ]
+
+    title = models.CharField(verbose_name='Заголовок', max_length=255)
+    price = models.PositiveIntegerField(verbose_name='Цена')
+    image = models.ImageField(verbose_name='Фото', upload_to=upload_path)
+    author = models.ForeignKey(User, verbose_name='Автор', on_delete=models.CASCADE)
+    city = models.CharField(verbose_name='Город', max_length=30)
+    condition = models.CharField(verbose_name='Состояние', max_length=3, choices=CONDITION, default='new')
+    fit_for = models.CharField(verbose_name='Подходит для', max_length=255)
+    description = models.CharField(verbose_name='Описание', max_length=450)
+    publication_date = models.DateTimeField(verbose_name='Дата публикации', auto_now_add=True)
+    type_of_transport = models.ForeignKey(TypeOfTransport, verbose_name='Тип транспорта', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Запчасть'
+        verbose_name_plural = 'Запчасти'
+
+    def __str__(self):
+        return f'{self.title} | {self.author}'
