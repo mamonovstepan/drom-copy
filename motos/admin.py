1f70370e2d60e81f5ef267737e24d0b887f6846b
Stepan Mamonov <stepan.mamonov.1406@gmail.com>
2024-01-25 06:58:11 +0800

    зарегистрировал модели в админ-панели

--- motos/admin.py
+++ motos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models as m
 
-# Register your models here.
+admin.site.register(m.MotorcycleClass)
+admin.site.register(m.MotorcycleBrand)
+admin.site.register(m.MotorcycleModel)
+
+
+@admin.register(m.MotorcyclePost)
+class MotoPostAdmin(admin.ModelAdmin):
+    list_display = ['moto_brand', 'moto_model', 'year_of_issue',
+                    'city','price', 'fuel', 'engine_capacity',
+                    'stroke', 'moto_class', 'mileage']
