ca49705b1d752f17060fddc93089a00d0966387b
Stepan Mamonov <stepan.mamonov.1406@gmail.com>
2024-01-25 05:45:52 +0800

    добавлены модели хранящие данные о мотоциклах (предварительная версия)

--- motos/models.py
+++ motos/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from cities.models import City
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Список хранящий тактность мотора мотоцикла
+STROKE = [
+    (2, '2-тактный'),
+    (4, '4-тактный'),
+]
+
+""" Модели для хранения информации о мотоциклах """
+
+
+# Модель для классификации мотоциклов
+class MotorcycleClass(models.Model):
+    name = models.CharField(verbose_name='Класс мотоцикла', max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Класс мотоцикла'
+        verbose_name_plural = 'Классы мотоциклов'
+
+
+# Класс для хранения производителя
+class MotorcycleBrand(models.Model):
+    name = models.CharField(verbose_name='Марка', max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Марка'
+        verbose_name_plural = 'Марки'
+
+
+# Класс для хранения моделей мотоциклов
+class MotorcycleModel(models.Model):
+    name = models.CharField(verbose_name='Модель', max_length=60, unique=True)
+    motorcicle_brand = models.ForeignKey(MotorcycleBrand, verbose_name='Марка',
+                                         related_name='brand', max_length=15,
+                                         on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Модель'
+        verbose_name_plural = 'Модели'
