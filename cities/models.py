4dc305cbded88e85d0e46d60dde6fb7273153d89
Stepan Mamonov <stepan.mamonov.1406@gmail.com>
2024-01-27 21:34:33 +0800

    города и области разделены на две разные модели

--- cities/models.py
+++ cities/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
 
+# Модель хранящая области страны
 class Region(models.Model):
     name = models.CharField(verbose_name='Область',
                               max_length=120, unique=True)
 
+    def __str__(self):
+        return self.name
 
+    class Meta:
+        verbose_name = 'Область'
+        verbose_name_plural = 'Области'
+
+
+# Модель хранящая города страны
 class City(models.Model):
     name = models.CharField(verbose_name='Город',
                             max_length=120, unique=True)
