269bd3c01b645abbcd9897a7ddbd3a31300b2401
Stepan Mamonov <stepan.mamonov.1406@gmail.com>
2024-02-10 19:40:12 +0800

    предварительная версия модели спецификаций автмобилей

--- specifications/models.py
+++ specifications/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from cars import models as m
 
-# Create your models here.
+
+class CarSpecification(models.Model):
+    car_barnd = models.ForeignKey(m.CarBrand, verbose_name='Марка авто', on_delete=models.PROTECT)
+    car_model = models.ForeignKey(m.CarModel, verbose_name='Модель авто', on_delete=models.PROTECT)
+    car_generation = models.ForeignKey(m.CarGeneration, verbose_name='Поколение', on_delete=models.PROTECT)
+    car_equipment = models.ForeignKey(m.CarEquipment, verbose_name='Комплектация', on_delete=models.PROTECT)
+    year_of_issue = models.models.PositiveSmallIntegerField(verbose_name='Год выпуска', validators=[MaxValueValidator(date.today().year), MinValueValidator(1900)])
