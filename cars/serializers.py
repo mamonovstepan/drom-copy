1006b7ca193bf1eed78717516a5a81bcffbc0ace
Stepan Mamonov <stepan.mamonov.1406@gmail.com>
2024-02-15 20:51:27 +0800

    исправил название сериализатора

--- cars/serializers.py
+++ cars/serializers.py
@@ -2,7 +2,7 @@ from .models import CarIcon
 from rest_framework import serializers
 
 
-class BrandsSerializer(serializers.ModelSerializer):
+class IconsSerializer(serializers.ModelSerializer):
     class Meta:
         model = CarIcon
         fields = ['name', 'car_brand']
